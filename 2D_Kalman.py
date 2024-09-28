629caa8f31f7d5fe32739d2572cdc9f0296a7d80
jinyong <jdragon0@naver.com>
2024-09-28 16:59:13 +0900

    Update 2D_Kalman.py

--- 2D_Kalman.py
+++ 2D_Kalman.py
@@ -2,13 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.interpolate as interp
 
-x = np.array([0,1,2,4,7,7,6,7,8,9,10,11,11,11])
+x = np.array([0,1,2,4,7,7,6,7,8,9,10,9,6,4])
 y = np.array([0,2,5,7,6,4,2,0,1,3,4,3,2,2])
 t = np.array([0,1,2,3,4,5,6,7,8,9,10,11,12,13])
 
 num = 50
 
-tt = np.linspace(0,13,num)
+tt = np.linspace(0,t[-1],num)
+dt = tt[1] - tt[0]
 
 fx = interp.interp1d(t,x,kind='quadratic')
 xx = fx(tt)
@@ -16,36 +17,41 @@ xx = fx(tt)
 fy = interp.interp1d(t,y,kind='quadratic')
 yy = fy(tt)
 
-w = np.random.randn(1,np.size(tt))*0.01
-v = np.random.randn(1,np.size(tt))*0.01
 
-A = np.array([[1, tt[1]],[0,1]])
-H = np.array([1,0])[np.newaxis]
-P = np.array([[1 ,0],[0,1]])
+A = np.matrix([[1,dt,dt*dt/2],[0,1,dt],[0,0,1]])
+H = np.matrix([1,0,0])
+P = np.eye(A.shape[0])
 
-R = 0.01 
-Q = 0.01 * np.array([[1 ,0],[0,1]]) 
+R = 0.1
+Q = np.eye(3)*0.1 
 
-initx = np.array([[0, 0],[xx[1],yy[1]]])
+initx = np.matrix([[0, 0],[0, 0],[0.1, 0.1]])
 xh = initx
 
 xxx = np.zeros(num)
 yyy = np.zeros(num)
-
+ex = np.zeros(num)
+ey = np.zeros(num)
 for k in range(num):
-    xhp = A@xh+w[0][k]
+    w = np.matrix(np.random.randn(3,2))* 0.1
+    xh = A@xh + w
     Pp = A@P@A.transpose()+Q
-    K = Pp@H.transpose()/(H@Pp@H.transpose()+R)
+    K = Pp@H.transpose()@np.linalg.inv(H@Pp@H.transpose()+R)
 
-    z = np.array([xx[k],yy[k]])+v[0][k]
+    v = np.matrix(np.random.randn(1,2)) *0.1
+    z = np.matrix([xx[k],yy[k]]) + v
 
-    xh = xhp+K@(z-H@xhp)
+    xh = xh+K@(z-H@xh)
     P = Pp-K@H@Pp
 
-    xxx[k] = xh[0][0]
-    yyy[k] = xh[0][1]
+    xxx[k] = xh.item(0)
+    yyy[k] = xh.item(1)
 
+    ex[k] = z.item(0)
+    ey[k] = z.item(1)
+
+plt.plot(xx,yy)
+plt.plot(ex,ey,'o')
 plt.plot(xxx,yyy,'x')
-plt.plot(xx,yy,'o')
 plt.grid()
 plt.show()
