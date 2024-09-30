34e23a4d0920309e60f4fd42e08d90b3951ff8a0
jinyong <jdragon0@naver.com>
2024-09-30 22:34:11 +0900

    Update 2D_Kalman.py

--- 2D_Kalman.py
+++ 2D_Kalman.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.interpolate as interp
 
-x = np.array([0,1,2,4,7,7,6,7,8,9,10,9,6,4])
-y = np.array([0,2,5,7,6,4,2,0,1,3,4,3,2,2])
+x = np.array([0,1,2,4,7,7,6,7,8,9,10,9,6,4])*10
+y = np.array([0,2,5,7,6,4,2,0,1,3,4,3,2,2])*10
 t = np.array([0,1,2,3,4,5,6,7,8,9,10,11,12,13])
 
-num = 1000
+num = 500
 
 tt = np.linspace(0,t[-1],num)
 dt = tt[1] - tt[0]
@@ -22,8 +22,8 @@ A = np.matrix([[1,dt,dt*dt/2],[0,1,dt],[0,0,1]])
 H = np.matrix([1,0,0])
 P = np.eye(A.shape[0])
 
-R = 10 # measurement noise covariance
-Q = np.eye(3) * 0.05
+R = 6 # measurement noise covariance
+Q = np.matrix([[dt**4/4,dt**3/2,dt**2/2],[dt**3/2,dt**2,dt],[dt**2/2,dt,1]])*1
 
 initx = np.matrix([[0, 0],[0, 0],[0.1, 0.1]])
 xh = initx
@@ -38,7 +38,7 @@ for k in range(num):
     Pp = A@P@A.transpose()+Q
     K = Pp@H.transpose()@np.linalg.inv(H@Pp@H.transpose()+R)
 
-    v = np.matrix(np.random.randn(1,2)) * 0.6
+    v = np.matrix(np.random.randn(1,2)) * 6
     z = np.matrix([xx[k],yy[k]]) + v
 
     xh = xh+K@(z-H@xh)
